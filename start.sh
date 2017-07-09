18c0a60b3d21936d381d5461db25f2695d485f28
HosseinAbedi <abedi.hossein@protonmail.ch>
2017-07-09 06:47:35 +0200

    just added some configurations

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 echo "Kicking off!"
 
-java -cp target/meliora-1.0-alpha-jar-with-dependencies.jar Trainer
+java -cp target/meliora-1.0-alpha-jar-with-dependencies.jar Trainer  -Xms1024m  -Xmx85g
 
