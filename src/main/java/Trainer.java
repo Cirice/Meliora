aac05cce30ffb8fb2376de97eb6b2b94bc0c835b
Hossein Abedi <abedi.hossein@protonmail.ch>
2017-07-29 00:17:36 +0430

    removing some files and adding some description to the config file

--- src/main/java/Trainer.java
+++ src/main/java/Trainer.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.Properties;
-
 import org.apache.log4j.Logger;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.word2vec.Word2Vec;
